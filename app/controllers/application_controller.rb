719672940826abd618d8ad162dad6c41142cc5b7
RubyistCTRLDYT <763396850@qq.com>
2017-06-02 09:23:09 +0800

    fix html,message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    str = "\nGotcha MotherFucker! 
-           \nLooking for my password ah? Shitty Dog LOL~
-           \nNo password here, you God damn stupid mother fucker LMAO~"
-    render text: str
-  end
 end
