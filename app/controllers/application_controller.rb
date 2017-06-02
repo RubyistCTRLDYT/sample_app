defd6f672ec2d2aaf37aaa5610ea2f1fb185419e
RubyistCTRLDYT <763396850@qq.com>
2017-06-02 09:06:22 +0800

    new message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    str = "\nGotcha MotherFucker! 
+                  \nLooking for my password ah? Shitty Dog ha~
+                  \nNo password here, you God damn stupid mother fucker LMAO~"
+    render text: str
   end
 end
